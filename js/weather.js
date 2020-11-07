038bbd7e176296c491b19e6e8e864ad0ca71c765
kvn <kvngard@hermes.dhcp.thefacebook.com>
2020-11-07 14:41:46 -0800

    Fix weather key and name

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '2cf89c1d53be8f70a040718c251ce13f';
 
 // Set Position function
 setPosition();
